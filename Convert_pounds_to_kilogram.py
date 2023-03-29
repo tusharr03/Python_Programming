531573b8a1e08ac33fe8f00e0f238e456cbb4ac5
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-29 16:39:42 +0000

    Add files via upload

---
+++ Convert_pounds_to_kilogram.py
@@ -0,0 +1,4 @@
+Weight_lbs = input ('Enter weight in pounds: ')
+Weight_kg = 0.45 * int ( Weight_lbs )
+int ( Weight_kg) #we can also print weight in kilograms without converting it to integer 
+print('Weight in kilograms is: ', Weight_kg)
