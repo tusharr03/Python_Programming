257d6c8e88739e0ad8b731a2f5df426077c93b36
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-29 16:39:58 +0000

    Add files via upload

---
+++ Introduction_to_string.py
@@ -0,0 +1,15 @@
+Str_1 = 'Introduction to string'
+print(Str_1) # Prints the String same as mentioned above
+Str_2 = input('Enter a String: ') 
+Str_3 = input("Enter a String: ") # for enter a string containing a single quote
+print(Str_2)
+print(Str_3)
+print(Str_1[0]) #Prints the first character of the String
+print(Str_1[-1]) #Prints the last character of the String
+Str_4 = '''
+By this method 
+We can print multiple lines of string 
+at a time
+'''
+print(Str_4)
+print(Str_1[0:4]) #Prints the string from the index 0 to 4 (excluding character at index 4)
