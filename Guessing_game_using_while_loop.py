2484b21d56edd18217141628676925cc9aee4d59
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 18:17:02 +0000

    Add files via upload

---
+++ Guessing_game_using_while_loop.py
@@ -0,0 +1,15 @@
+secret_number=9
+guess_count=0
+guess_limit=3
+while guess_count <  guess_limit:
+    guess=int(input("Guess: "))
+    guess_count+=1
+    if guess==secret_number:
+        print('You Won!')
+        break
+    print("Try Again")
+else:
+    print('Wrong Guess!, Limit Exceeded')
+
+
+    
