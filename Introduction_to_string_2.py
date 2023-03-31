fa1724afa8a8e3268bb3741e1f5fe41f36d61f14
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 18:15:50 +0000

    Add files via upload

---
+++ Introduction_to_string_2.py
@@ -0,0 +1,11 @@
+Name='Tushar Sharma'
+print(len(Name)) #prints the number of character in the given string
+print(Name.upper()) #print all character in upper class
+Name.find('a') #print the index of the first occurence of the character
+Name.find('Sharma') #print the index of the first character from which the word is starting
+Name_1=Name.replace('Sharma','Singh') #replace the first string or character with the string or character given
+print(Name.replace('Sharma','Singh'))
+print(Name_1)
+print('Tushar' in Name) #prints true if the given string is present in your previous string
+print('tushar' in Name) #prints false if the given string is not present in your previous string
+
