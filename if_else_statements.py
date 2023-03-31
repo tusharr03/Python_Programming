7a3ef8381eb57ae1da29375667599f99c957a75d
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 18:16:05 +0000

    Add files via upload

---
+++ if_else_statements.py
@@ -0,0 +1,17 @@
+is_hot=True
+is_cold=True
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold:
+    print("It's a cold day")
+    print("wear warn clothes")
+    
+else:
+    print("It's a lovely day")
+
+print("Enjoy your day")
+    
+
