7cc0359b9b50e1b945dacf49d352caf8601ecfb0
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 21:30:09 +0000

    Add files via upload

---
+++ Lists_in_python.py
@@ -0,0 +1,7 @@
+names=['suzuki','tata','chevrolet','renault','hyundai']
+print(names[0])#print the first string from the list
+print(names[0:3])#print the strings from index 0 to index 3 excluding string which has index 3
+print(names[-2])#print second string from the last
+print(names[2:])#print string from index 2 to the last
+names[0]='maruti suzuki'
+print(names)
