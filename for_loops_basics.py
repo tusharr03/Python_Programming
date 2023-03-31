101e8cbd4c2bd456a2c41ac592fe755a4c471b6a
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 21:29:35 +0000

    Add files via upload

---
+++ for_loops_basics.py
@@ -0,0 +1,18 @@
+for name in 'Tushar': #print all the character one by one present in the string and name is the loop variable here
+    print(name)
+print('\n')
+for number in [1,2,3,4,5,6]:#print all the numbers one by one present in the square bracket and number is a loop variable here
+    print(number)
+print('\n')
+for names in ['Tushar', 'Yash', 'Anant', 'Divyanshu']:#prints all the names one by one present in the square bracket and names is the loop variable here
+    print(names)
+print('\n')
+for numbers in range(10): #print all the number in the given range and range is the pre-defined function in for loop
+    print(numbers)
+print('\n')
+for num in range(5,10) : #print all the numbers between the range 5 to 10
+    print(num)
+print('\n')
+for num_1 in range (5,10,2): #print all the number between the range 5 to 10 with a difference of 2
+    print(num_1)
+print('\n')
