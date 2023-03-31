1702a2f567b70468bdaecb3258884125e1aad35b
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-04-01 00:44:00 +0530

    Update Car_simulation_games_using_basic_loops.py

--- Car_simulation_games_using_basic_loops.py
+++ Car_simulation_games_using_basic_loops.py
@@ -1,10 +1,19 @@
 command =""
+is_started=False
 while True:
     command=input("> ").lower()
     if command == "start" :
-        print("Car Started... Ready to go")
+        if is_started == True:
+            print("Car is already started.")
+        else :
+            is_started=True
+            print("Car Started... Ready to go")
     elif command == "stop" :
-        print("Car Stopped")
+        if not is_started == True:
+            print("Car is already stopped")
+        else :    
+            is_started=False
+            print("Car Stopped")
     elif command == "help" :
         print('''
         start-> to start the car
