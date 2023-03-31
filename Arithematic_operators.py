2d494c0b1d4acc2db1baa98f4d015f10bdef84cf
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2023-03-31 18:14:52 +0000

    Add files via upload

---
+++ Arithematic_operators.py
@@ -0,0 +1,23 @@
+print(10/3) #returns the float value after division 
+print(100/4)
+print(10//3) #by using // prints the integer value after division
+print(10%3) #prints the remainder after the division
+print(10**3) #prints the result of 10 to the power 3
+#increamenting a variable
+x=3
+x=x+15
+print(x)
+x+=2
+print(x)
+#operator precedence
+# first precedence is for parenthesis[()]
+# second precedence is for exponentiation (**)
+# third precedence is for Multiplication(*) or Division(/ or //)
+# fourth precedence is for Addition(+) or Subtraction(-)
+x=10+3*6
+print(x)
+# round function
+x=2.6
+print(round(x))
+# abs function
+print(abs(-7.4)) #prints always the poisitive value of any variable or number given
